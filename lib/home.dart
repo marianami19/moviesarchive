316217208890206f4ac67c6e2e53bfc0c52852b3
marianami19 <nelsonmaria98@gmail.com>
2023-08-17 20:48:48 +0530

    sorting and searching

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,12 @@ class HomeScreen extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+enum MovieSortOption {
+  Year,
+  Popularity,
+  IMDBRating,
+}
+
 class _HomePageState extends State<HomeScreen> {
   List<Movies> topRatedMovies = [];
   List<Movies> favoriteMovies = [];
@@ -15,6 +21,32 @@ class _HomePageState extends State<HomeScreen> {
   int _currentIndex = 0;
   int _currentPage = 1;
   int _pageSize = 5;
+  TextEditingController _searchController = TextEditingController();
+
+  MovieSortOption _currentSortOption = MovieSortOption.Year;
+
+  List<Movies> sortMovies(List<Movies> movies) {
+    switch (_currentSortOption) {
+      case MovieSortOption.Year:
+        return [...movies]
+          ..sort((a, b) => b.releaseDate.compareTo(a.releaseDate));
+      case MovieSortOption.Popularity:
+        return [...movies]
+          ..sort((a, b) => b.popularity.compareTo(a.popularity));
+      case MovieSortOption.IMDBRating:
+        return [...movies]
+          ..sort((a, b) => b.voteAverage.compareTo(a.voteAverage));
+      default:
+        return movies;
+    }
+  }
+
+  List<Movies> searchMovies(String query) {
+    query = query.toLowerCase();
+    return topRatedMovies
+        .where((movie) => movie.title.toLowerCase().contains(query))
+        .toList();
+  }
 
   @override
   void initState() {
@@ -63,53 +95,286 @@ class _HomePageState extends State<HomeScreen> {
   }
 
   Widget buildTopRatedMoviesPage() {
-    return LazyLoadListView(
-      items: topRatedMovies,
-      itemBuilder: (context, index, movie) {
-        return ListTile(
-          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          title: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    List<Movies> filteredMovies = _searchController.text.isNotEmpty
+        ? searchMovies(_searchController.text)
+        : topRatedMovies;
+
+    // Apply sorting based on the selected sort option
+    switch (_currentSortOption) {
+      case MovieSortOption.Year:
+        filteredMovies.sort((a, b) => a.releaseDate.compareTo(b.releaseDate));
+        break;
+      case MovieSortOption.Popularity:
+        filteredMovies.sort((a, b) => b.popularity.compareTo(a.popularity));
+        break;
+      case MovieSortOption.IMDBRating:
+        filteredMovies.sort((a, b) => b.voteAverage.compareTo(a.voteAverage));
+        break;
+    }
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: TextField(
+            controller: _searchController,
+            onChanged: (query) {
+              setState(() {});
+            },
+            decoration: InputDecoration(
+              hintText: 'Search movies by name',
+              prefixIcon: Icon(Icons.search),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
             children: [
-              Container(
-                width: 100,
-                height: 150,
-                child: Image.network(
-                  'https://image.tmdb.org/t/p/w200${movie.posterPath}',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(movie.title,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
-                    Text('Year: ${extractYearFromDate(movie.releaseDate)}'),
-                    Text('IMDB Rating: ${movie.voteAverage}'),
-                  ],
-                ),
-              ),
-              IconButton(
-                icon: Icon(Icons.favorite),
-                color:
-                    favoriteMovies.contains(movie) ? Colors.red : Colors.grey,
-                onPressed: () {
-                  toggleFavorite(movie);
+              Text('Sort by: '),
+              DropdownButton<MovieSortOption>(
+                value: _currentSortOption,
+                onChanged: (option) {
+                  setState(() {
+                    _currentSortOption = option!;
+                  });
                 },
+                items: MovieSortOption.values
+                    .map<DropdownMenuItem<MovieSortOption>>(
+                  (MovieSortOption option) {
+                    return DropdownMenuItem<MovieSortOption>(
+                      value: option,
+                      child: Text(option.toString().split('.').last),
+                    );
+                  },
+                ).toList(),
               ),
             ],
           ),
-          onTap: () {
-            // Add your logic for handling tap on the movie tile
-          },
-        );
-      },
-      onLoadMore: _loadMoreTopRatedMovies,
+        ),
+        Expanded(
+          child: LazyLoadListView(
+            items: filteredMovies,
+            itemBuilder: (context, index, movie) {
+              return ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                title: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 150,
+                      child: Image.network(
+                        'https://image.tmdb.org/t/p/w200${movie.posterPath}',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(movie.title,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Text(
+                              'Year: ${extractYearFromDate(movie.releaseDate)}'),
+                          Text('IMDB Rating: ${movie.voteAverage}'),
+                        ],
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      color: favoriteMovies.contains(movie)
+                          ? Colors.red
+                          : Colors.grey,
+                      onPressed: () {
+                        toggleFavorite(movie);
+                      },
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  // Add your logic for handling tap on the movie tile
+                },
+              );
+            },
+            onLoadMore: _loadMoreTopRatedMovies,
+          ),
+        ),
+      ],
     );
   }
+
+// Widget buildTopRatedMoviesPage() {
+//     List<Movies> filteredMovies = _searchController.text.isNotEmpty
+//         ? searchMovies(_searchController.text)
+//         : topRatedMovies;
+//   List<Movies> sortedMovies = sortMovies(filteredMovies);
+
+//   return Column(
+//     children: [
+//       Padding(
+//         padding: EdgeInsets.all(16),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             DropdownButton<MovieSortOption>(
+//               value: _currentSortOption,
+//               onChanged: (newSortOption) {
+//                 setState(() {
+//                   _currentSortOption = newSortOption!;
+//                 });
+//               },
+//               items: MovieSortOption.values.map<DropdownMenuItem<MovieSortOption>>((option) {
+//                 return DropdownMenuItem<MovieSortOption>(
+//                   value: option,
+//                   child: Text(option.toString().split('.').last),
+//                 );
+//               }).toList(),
+//             ),
+//             Text('Sort by: ${_currentSortOption.toString().split('.').last}'),
+//           ],
+//         ),
+//       ),
+//       Expanded(
+//         child: LazyLoadListView(
+//           items: sortedMovies,
+//           itemBuilder: (context, index, movie) {
+//             // The rest of your movie item UI
+//             // ...
+//           },
+//           onLoadMore: _loadMoreTopRatedMovies,
+//         ),
+//       ),
+//     ],
+//   );
+// }
+
+//   Widget buildTopRatedMoviesPage() {
+//     List<Movies> filteredMovies = _searchController.text.isNotEmpty
+//         ? searchMovies(_searchController.text)
+//         : topRatedMovies;
+
+//     return Column(
+//       children: [
+//         Padding(
+//           padding: EdgeInsets.all(16),
+//           child: TextField(
+//             controller: _searchController,
+//             onChanged: (query) {
+//               setState(() {});
+//             },
+//             decoration: InputDecoration(
+//               hintText: 'Search movies by name',
+//               prefixIcon: Icon(Icons.search),
+//             ),
+//           ),
+//         ),
+//         Expanded(
+//           child: LazyLoadListView(
+//             items: filteredMovies,
+//             itemBuilder: (context, index, movie) {
+//               return ListTile(
+//                 contentPadding:
+//                     EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+//                 title: Row(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Container(
+//                       width: 100,
+//                       height: 150,
+//                       child: Image.network(
+//                         'https://image.tmdb.org/t/p/w200${movie.posterPath}',
+//                         fit: BoxFit.cover,
+//                       ),
+//                     ),
+//                     SizedBox(width: 16),
+//                     Expanded(
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text(movie.title,
+//                               style: TextStyle(
+//                                   fontSize: 16, fontWeight: FontWeight.bold)),
+//                           Text(
+//                               'Year: ${extractYearFromDate(movie.releaseDate)}'),
+//                           Text('IMDB Rating: ${movie.voteAverage}'),
+//                         ],
+//                       ),
+//                     ),
+//                     IconButton(
+//                       icon: Icon(Icons.favorite),
+//                       color: favoriteMovies.contains(movie)
+//                           ? Colors.red
+//                           : Colors.grey,
+//                       onPressed: () {
+//                         toggleFavorite(movie);
+//                       },
+//                     ),
+//                   ],
+//                 ),
+//                 onTap: () {
+//                   // Add your logic for handling tap on the movie tile
+//                 },
+//               );
+//             },
+//             onLoadMore: _loadMoreTopRatedMovies,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+
+  // Widget buildTopRatedMoviesPage() {
+  //   return LazyLoadListView(
+  //     items: topRatedMovies,
+  //     itemBuilder: (context, index, movie) {
+  //       return ListTile(
+  //         contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+  //         title: Row(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             Container(
+  //               width: 100,
+  //               height: 150,
+  //               child: Image.network(
+  //                 'https://image.tmdb.org/t/p/w200${movie.posterPath}',
+  //                 fit: BoxFit.cover,
+  //               ),
+  //             ),
+  //             SizedBox(width: 16),
+  //             Expanded(
+  //               child: Column(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   Text(movie.title,
+  //                       style: TextStyle(
+  //                           fontSize: 16, fontWeight: FontWeight.bold)),
+  //                   Text('Year: ${extractYearFromDate(movie.releaseDate)}'),
+  //                   Text('IMDB Rating: ${movie.voteAverage}'),
+  //                 ],
+  //               ),
+  //             ),
+  //             IconButton(
+  //               icon: Icon(Icons.favorite),
+  //               color:
+  //                   favoriteMovies.contains(movie) ? Colors.red : Colors.grey,
+  //               onPressed: () {
+  //                 toggleFavorite(movie);
+  //               },
+  //             ),
+  //           ],
+  //         ),
+  //         onTap: () {
+  //           // Add your logic for handling tap on the movie tile
+  //         },
+  //       );
+  //     },
+  //     onLoadMore: _loadMoreTopRatedMovies,
+  //   );
+  // }
 
 // Helper function to extract year from a date string (YYYY-MM-DD)
   String extractYearFromDate(String dateString) {
